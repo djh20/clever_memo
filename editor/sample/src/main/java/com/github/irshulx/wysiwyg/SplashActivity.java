6eb3e357506448c933e4f144d93f68f6371a7205
HJ0508 <51086651+HJ0508@users.noreply.github.com>
2020-02-19 01:22:50 +0900

    splashScreen
    change splashScreen

--- editor/sample/src/main/java/com/github/irshulx/wysiwyg/SplashActivity.java
+++ editor/sample/src/main/java/com/github/irshulx/wysiwyg/SplashActivity.java
@@ -2,8 +2,12 @@ package com.github.irshulx.wysiwyg;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.github.irshulx.wysiwyg.Database.DatabaseManager;
 import com.github.irshulx.wysiwyg.NLP.NLPManager;
@@ -14,11 +18,28 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Twitter twitter = new Twitter(); // 트위터 객체 생성 및 put
-        NLPManager nlpManager = NLPManager.getInstance(twitter);
-        DatabaseManager databaseManager = DatabaseManager.getInstance(getApplicationContext());
-        Intent intent = new Intent(this, FirstActivity.class);
-        startActivity(intent);
-        finish();
+        setContentView(R.layout.activity_splashscreen);
+        Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+
+                ImageView logo = (ImageView) findViewById(R.id.logo);
+                Animation splashAnim = AnimationUtils.loadAnimation(SplashActivity.this, R.anim.splash_animation_show);
+                logo.startAnimation(splashAnim);
+                Handler handler = new Handler();
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Twitter twitter = new Twitter(); // 트위터 객체 생성 및 put
+                        NLPManager nlpManager = NLPManager.getInstance(twitter);
+                        DatabaseManager databaseManager = DatabaseManager.getInstance(SplashActivity.this);
+                        Intent intent = new Intent(SplashActivity.this, FirstActivity.class);
+                        startActivity(intent);
+                        SplashActivity.this.finish();
+                    }
+                });
+            }
+        });
     }
 }
